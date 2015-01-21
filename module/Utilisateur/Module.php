6236e296a46ffe4c1d917c56a4ad487391d38b34
NeoLied <cini.tristan@gmail.com>
2015-01-21 11:37:22 +0100

    Correction des fichiers de configuration de "Utilisateur"

--- module/Utilisateur/Module.php
+++ module/Utilisateur/Module.php
@@ -1,25 +1,26 @@
-<?php
-
 namespace Utilisateur;
 
-class Utilisateur
-{
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
+ use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+ use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-}
+ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+ {
+     public function getAutoloaderConfig()
+     {
+         return array(
+             'Zend\Loader\ClassMapAutoloader' => array(
+                 __DIR__ . '/autoload_classmap.php',
+             ),
+             'Zend\Loader\StandardAutoloader' => array(
+                 'namespaces' => array(
+                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                 ),
+             ),
+         );
+     }
+
+     public function getConfig()
+     {
+         return include __DIR__ . '/config/module.config.php';
+     }
+ }
