69c153e1b3cc7f3b97cf79646360f975e9831a50
Benjiz31 <benjizti31@gmail.com>
2015-01-22 14:29:06 +0100

    Modif application config
    Ajout SanAuth

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,8 @@ return array(
     'modules' => array(
         'Application',
     	'Utilisateur',
-    	'Annonce',			
+    	'Annonce',
+    	'SanAuth'			
     ),
 
     // These are various options for the listeners attached to the ModuleManager
